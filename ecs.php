d41bb3f21453214d7d33235b1033e9348293b89f
Sam Mousa <sam@mousa.nl>
2023-05-09 15:59:11 +0200

    chore: cleanup

--- ecs.php
+++ ecs.php
@@ -13,10 +13,8 @@ return static function (ECSConfig $ecsConfig): void {
 
     // Paths
     $ecsConfig->paths([
-        __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php'
-
-
-
+        __DIR__ . '/src',
+        __DIR__ . '/ecs.php'
     ]);
     // A. full sets
     $ecsConfig->import(SetList::PSR_12);
