e7fb59f7b533c9cabd9a38059b8080330195b430
Sam <sam@mousa.nl>
2026-05-27 11:39:15 +0200

    fix: update deps

--- src/OAuth2AuthenticationException.php
+++ src/OAuth2AuthenticationException.php
@@ -15,7 +15,7 @@ class OAuth2AuthenticationException extends \Exception
     public function __construct(
         public readonly OAuth2AuthenticationError $error,
         public readonly null|UriInterface $errorUri = null,
-        string $message = null
+        null|string $message = null
     ) {
         parent::__construct($message ?? $error->description());
     }
